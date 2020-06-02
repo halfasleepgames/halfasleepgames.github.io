496a93eb109647f58de217dd332449974ea19d50
davidh <hyhdvd@gmail.com>
2020-06-01 19:43:29 -0700

    setup site

---
+++ scripts/scripts.js
@@ -0,0 +1,26 @@
+sr.reveal('.js-fade-load',
+{
+  distance: '7.5rem',
+  scale: 1,
+  viewFactor: .1,
+  duration: 600,
+  opacity: 0
+}, 100);
+
+sr.reveal('.js-fade-load2',
+{
+  distance: '6rem',
+  scale: 1,
+  viewFactor: .1,
+  duration: 600,
+  opacity: 0,
+  delay: 150
+}, 100);
+
+sr.reveal('.js-fade-content',
+{
+  distance: '7.5rem',
+  scale: 1,
+  viewFactor: 0.33,
+  opacity: 0
+});
