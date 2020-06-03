1f7c43874b99c9f09f7eedb348e167a08db0c29c
davidh <hyhdvd@gmail.com>
2020-06-02 21:18:32 -0700

    uu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,22 +5,4 @@ sr.reveal('.js-fade-load',
   viewFactor: .1,
   duration: 600,
   opacity: 0
-}, 100);
-
-sr.reveal('.js-fade-load2',
-{
-  distance: '6rem',
-  scale: 1,
-  viewFactor: .1,
-  duration: 600,
-  opacity: 0,
-  delay: 150
-}, 100);
-
-sr.reveal('.js-fade-content',
-{
-  distance: '7.5rem',
-  scale: 1,
-  viewFactor: 0.33,
-  opacity: 0
-});
+}, 33);
