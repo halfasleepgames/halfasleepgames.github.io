6b2918c6dd9c63750a54c912740ebcd6bb27b2c9
davidh <hyhdvd@gmail.com>
2020-10-03 14:01:56 -0700

    u

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,16 @@
 sr.reveal('.js-reveal',
 {
   delay: 50,
+  distance: '0',
+  scale: 1,
+  viewFactor: .1,
+  duration: 1000,
+  opacity: 0
+}, 100);
+
+sr.reveal('.js-slide',
+{
+  delay: 50,
   distance: '6rem',
   scale: 1,
   viewFactor: .1,
