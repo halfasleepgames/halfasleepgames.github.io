8f5ea405325790b70adea0f0fa131a3365f22ff5
davidh <hyhdvd@gmail.com>
2020-10-04 11:35:52 -0700

    u

---
+++ test/scripts/scripts.js
@@ -0,0 +1,19 @@
+sr.reveal('.js-reveal',
+{
+  delay: 420,
+  distance: '0',
+  scale: 1,
+  viewFactor: .1,
+  duration: 1000,
+  opacity: 0
+}, 100);
+
+sr.reveal('.js-slide',
+{
+  delay: 50,
+  distance: '6rem',
+  scale: 1,
+  viewFactor: .1,
+  duration: 1000,
+  opacity: 0
+}, 100);
